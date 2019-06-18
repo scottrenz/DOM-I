7aeb20894942606e30a9e2b80d4a1ae34cdf779b
Scott Renz <scottrenz@scottrenz.com>
2019-06-17 20:32:19 -0400

    DOM I nav color green

--- js/index.js
+++ js/index.js
@@ -77,9 +77,11 @@ console.log(siteContent);
 console.log(siteContent.nav);
 console.log(siteContent.nav['nav-item-1']);
 // myA.forEach
-for (let i = 0; i < 6; i++) {
-  myNav.childNodes[i].textContent = siteContent.nav['nav-item-' + (i+1)]
-  console.log('mynav ', myNav.childNodes[i].textContent );
+console.log(myNav.childNodes)   // textContent.style.color = 'green';
+for (let i = 0; i < myA.length; i++) {
+  myA[i].textContent = siteContent.nav['nav-item-' + (i+1)]
+  myA[i].style.color = 'green';
+  console.log('mynav ', myA[i].textContent );
 }
 myCtaText.textContent = siteContent.cta.h1;
 console.log(myCtaText)
@@ -106,59 +108,23 @@ console.log(myBottomContent.childNodes)
 
 const multiH4 = document.querySelectorAll('h4');
 console.log(multiH4);
-// multiH4[0].textContent = 'a0'
-// multiH4[1].textContent = 'a1'
-// multiH4[2].textContent = 'a2'
-// multiH4[3].textContent = 'a3'
-// multiH4[4].textContent = 'a4'
-// multiH4[5].textContent = 'a5'
 
-// multiH4[4].textContent = siteContent["main-content"]["vision-h4"]
+multiH4[4].textContent = siteContent["main-content"]["vision-h4"]
+multiH4[5].textContent = siteContent["contact"]["contact-h4"]
 
+const multiP = document.querySelectorAll('p');
+console.log(multiP);
 
-// "services-h4":"Services",
-// "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-// "product-h4":"Product",
-// "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-// "vision-h4":"Vision",
-// "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+multiP[4].textContent = siteContent["main-content"]["vision-content"]
+multiP[5].textContent = siteContent["contact"]["address"]
+multiP[6].textContent = siteContent["contact"]["phone"]
+multiP[7].textContent = siteContent["contact"]["email"]
 
-// for (let i = 0; i < 2; i++) {
-//   // myTopContent.childNodes[i].textContent = siteContent.nav['nav-item-' + (i+1)]
-//   console.log(siteContent.['nav-item-' + (i+1)]
-//   console.log('mytop ', myTopContent.childNodes[i].textContent );
+multiP[8].textContent = siteContent["footer"]["copyright"]
 
-/* <section class="main-content">
+console.log(multiP[4])
+console.log(multiP[5])
+console.log(multiP[6])
+console.log(multiP[7])
 
-<div class="top-content">
-    <div class="text-content">
-        <h4></h4>
-        <p></p>
-    </div>
-    <div class="text-content">
-        <h4></h4>
-        <p></p>
-    </div>
-</div> */
-
-// "main-content": {
-//   "features-h4":"Features",
-//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "about-h4":"About",
-//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "middle-img-src": "img/mid-page-accent.jpg",
-//   "services-h4":"Services",
-//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "product-h4":"Product",
-//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "vision-h4":"Vision",
-//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-// },
-
-// myCta.childNodes[1].textContent = "Get Started";
-// console.log(myCta.childNodes[1])
-// "cta": {
-//   "h1": "DOM Is Awesome",
-//   "button": "Get Started",
-//   "img-src": "img/header-img.png"
-// },
+console.log(multiP[8])
